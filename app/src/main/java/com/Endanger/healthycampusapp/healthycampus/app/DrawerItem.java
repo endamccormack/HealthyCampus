344fb382dd6afc3f88c494993255caec8b019ff7
Enda McCormack <endamccormack0@gmail.com>
2014-04-01 16:33:42 +0100

    complete overhaul of the basics, finished nav bar

---
+++ app/src/main/java/com/Endanger/healthycampusapp/healthycampus/app/DrawerItem.java
@@ -0,0 +1,28 @@
+package com.Endanger.healthycampusapp.healthycampus.app;
+
+/**
+ * Created by Enda on 01/04/2014.
+ */
+public class DrawerItem {
+    String ItemName;
+    int imgResID;
+
+    public DrawerItem(String itemName, int imgResID) {
+        super();
+        ItemName = itemName;
+        this.imgResID = imgResID;
+    }
+
+    public String getItemName() {
+        return ItemName;
+    }
+    public void setItemName(String itemName) {
+        ItemName = itemName;
+    }
+    public int getImgResID() {
+        return imgResID;
+    }
+    public void setImgResID(int imgResID) {
+        this.imgResID = imgResID;
+    }
+}
